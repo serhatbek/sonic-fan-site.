833d2d892ad860cfbfeffff59e022027e67b6f77
serhatbek <serhat.bekk@gmail.com>
2020-09-22 21:22:25 +0300

    signin/login form modals with validation completed

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -73,3 +73,104 @@ function accordionVisible(e) {
     target.classList.remove('active');
   }
 }
+
+//**************** MAIN-NAV-LINK COLORED SHADOWS ************************
+
+const mainNavLink = document.querySelectorAll('.main-nav__list-item-link');
+
+mainNavLink.forEach((item) => {
+  const linkText = item.innerHTML;
+  const parent = item.parentElement;
+
+  const linkShadow1 = document.createElement('span');
+  linkShadow1.innerHTML = linkText;
+  linkShadow1.classList.add('main-nav__list-item-shadow1');
+  parent.appendChild(linkShadow1);
+
+  const linkShadow2 = document.createElement('span');
+  linkShadow2.innerHTML = linkText;
+  linkShadow2.classList.add('main-nav__list-item-shadow2');
+  parent.appendChild(linkShadow2);
+});
+
+// *************** DROPDOWN MENU ************************
+
+const dropDownPanel = document.querySelector('.js-dropdown-panel');
+const dropDownBtn = document.querySelector('.js-dropdown-btn');
+const btnParent = document.querySelector('.main-nav__list-item');
+
+dropDownBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  openDropdownMenu(e);
+});
+
+function openDropdownMenu() {
+  dropDownPanel.classList.toggle('active');
+  btnParent.classList.toggle('active');
+}
+
+window.addEventListener('load', () => {
+  document.addEventListener('click', (e) => {
+    onClickDocument(e);
+  });
+});
+
+function closeDropdownMenu() {
+  dropDownPanel.classList.remove('active');
+}
+
+function onClickDocument(e) {
+  const target = e.target;
+
+  if (dropDownPanel.classList.contains('active')) {
+    if (!dropDownPanel.contains(target) && !dropDownBtn.contains(target)) {
+      closeDropdownMenu();
+    }
+  }
+}
+
+//************* SIGN IN BUTTON WITH FORM MODAL *******************
+
+const openSignInBtn = document.querySelector('.btn__signIn');
+const closeSignInModalBtn = document.querySelector('.close-signIn');
+
+openSignInBtn.addEventListener('click', () => {
+  openSignInModal();
+});
+
+function openSignInModal() {
+  const formModalBox = document.querySelector('.form-modal-wrapper');
+  formModalBox.classList.add('active');
+}
+
+closeSignInModalBtn.addEventListener('click', () => {
+  closeSignInModal();
+});
+
+function closeSignInModal() {
+  const formModalBox = document.querySelector('.form-modal-wrapper');
+  formModalBox.classList.remove('active');
+}
+
+//************* LOGIN IN BUTTON WITH FORM MODAL *******************
+
+const openLogInBtn = document.querySelector('.btn__login');
+const closeLogInModalBtn = document.querySelector('.close-logIn');
+
+openLogInBtn.addEventListener('click', () => {
+  openLogInModal();
+});
+
+closeLogInModalBtn.addEventListener('click', () => {
+  closeLogInModal();
+});
+
+function openLogInModal() {
+  const formModalBox = document.querySelector('.login-modal');
+  formModalBox.classList.add('active');
+}
+
+function closeLogInModal() {
+  const formModalBox = document.querySelector('.login-modal');
+  formModalBox.classList.remove('active');
+}
